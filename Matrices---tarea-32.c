16fdf85a7c96c07cd37955a984a163a976363136
William Elí O.L <ramun346416@gmail.com>
2022-11-09 16:14:26 -0600

    Tarea_32

---
+++ Matrices---tarea-32.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+   	int x, i, deseis[6], detres[3];
+	/* rellenamos la matriz */
+	for (x = 0;x < 3;x++)
+	{
+		printf("\n");
+		for (i = 0;i < 3;i++)
+		{
+			printf("\t\t [ %d ] [ %d ] =0 \t\t", i, x);
+		}
+
+	}
+     printf("\n");   
+   	for (x = 0;x < 3;x++)
+	{
+		printf("\n");
+		for (i = 0;i < 3;i++)
+		{
+		printf("\t\t [ %d ] [ %d ] =0 \t\t", i, x);
+		}
+
+	}
+   
+
+    return 0;
+}
